82e7ced24616621707533bb0eef652a04873c793
Ishraq Hasan <ishraq10199@gmail.com>
2025-07-27 05:37:40 +0600

    #Foundation-003: Fonts, inputs, timers, and jumps - untested

--- js/main.js
+++ js/main.js
@@ -6,6 +6,62 @@ chip8.debug = false;
 
 chip8.quirks = Object.create(null);
 chip8.quirks.useVYinShifts = false;
+chip8.quirks.jumpWithOffsetAlt = false;
+
+chip8.input = (() => {
+  const keys = new Uint8Array(16);
+  const keyMap = new Array(16);
+  const mapKey = Object.create(null);
+
+  keyMap[0x1] = 'Digit1';
+  keyMap[0x2] = 'Digit2';
+  keyMap[0x3] = 'Digit3';
+  keyMap[0xc] = 'Digit4';
+
+  keyMap[0x4] = 'KeyQ';
+  keyMap[0x5] = 'KeyW';
+  keyMap[0x6] = 'KeyE';
+  keyMap[0xd] = 'KeyR';
+
+  keyMap[0x7] = 'KeyA';
+  keyMap[0x8] = 'KeyS';
+  keyMap[0x9] = 'KeyD';
+  keyMap[0xe] = 'KeyF';
+
+  keyMap[0xa] = 'KeyZ';
+  keyMap[0x0] = 'KeyX';
+  keyMap[0xb] = 'KeyC';
+  keyMap[0xf] = 'KeyV';
+
+  // @todo Register key events
+  // It should update `keys[key]` if pressed/released
+  // Released state value should be 0, and pressed state should be 1
+  for (let i = 0; i < 0xF; i++) {
+    mapKey[keyMap[i]] = i;
+    window.addEventListener('keydown', ({code: key}) => {
+      if (mapKey[key] === undefined) {
+        // Should add behavior here for kb shortcuts, if needed
+        return;
+      }
+      keys[mapKey[key]] = 1;
+    });
+    window.addEventListener('keyup', ({code: key}) => {
+      if (mapKey[key] === undefined) {
+        // Should add behavior here for kb shortcuts, if needed
+        return;
+      }
+      keys[mapKey[key]] = 0;
+    });
+  }
+
+
+  const ns = Object.create(null);
+  ns.isKeyPressed = (key) => {
+    return keys[key];
+  };
+
+  return ns;
+})();
 
 chip8.registers = (() => {
   const ns = Object.create(null);
@@ -28,6 +84,8 @@ chip8.stack = (() => {
 chip8.memory = new Uint8Array(4096);
 
 chip8.memoryUtils = (() => {
+  const fontMap = new Array(16);
+
   // @todo Program Counter, Index register operations here
   const ns = Object.create(null);
   ns.populateFonts = () => {
@@ -51,19 +109,28 @@ chip8.memoryUtils = (() => {
       [0xf0, 0x80, 0xf0, 0x80, 0x80], // F
     ];
     // By popular convention, we are using the memory from 0x050 (80) to 0x09F (160)
+    const fontStart = 0x050;
+
     for (
-      let i = 0x050, currentCharRow = 0, currentCharCol = 0;
-      i <= 0x09f;
-      i++, currentCharCol++
+      let i = fontStart, currentCharRow = 0, currentCharCol = 0;
+      i < (fontStart + 80);
+      i++
     ) {
+      if (currentCharCol === 0) {
+        fontMap[currentCharRow] = i;
+      }
       chip8.memory[i] = bitmaps[currentCharRow][currentCharCol];
 
-      if (currentCharCol == 5) {
+      if (currentCharCol === 4) {
         currentCharCol = 0;
         currentCharRow++;
       }
+      else {
+        currentCharCol++;
+      }
     }
   };
+  ns.fontMap = fontMap;
   return ns;
 })();
 
@@ -129,6 +196,29 @@ chip8.timer = (() => {
 
   // @todo Timer logic
   const ns = Object.create(null);
+
+  Object.defineProperty(ns, 'delay', {
+    get() {
+      return timers[DELAY];
+    },
+    set(value) {
+      timers[DELAY] = value;
+    },
+    configurable: false,
+    enumerable: false,
+  });
+
+  Object.defineProperty(ns, 'sound', {
+    get() {
+      return timers[SOUND];
+    },
+    set(value) {
+      timers[SOUND] = value;
+    },
+    configurable: false,
+    enumerable: false,
+  });
+
   return ns;
 })();
 
@@ -245,6 +335,41 @@ chip8.cpu = (() => {
     chip8.registers.V[registerX] = chip8.registers.V[registerX] << 1;
   };
 
+  ns.operations.jumpWithOffset = (address, register) => {
+    // Jumps to NNN + V[0]
+    chip8.cpu.PC = address + chip8.registers.V[register || 0];
+  };
+
+  ns.operations.generateRandomNumber = (register, value) => {
+    chip8.registers.V[register] = Math.floor(Math.random() * 256) & value;
+  };
+
+  ns.operations.readDelayTimer = (register) => {
+    chip8.registers.V[register] = chip8.timer.delay; 
+  };
+
+  ns.operations.writeDelayTimer = (register) => {
+    chip8.timer.delay = chip8.registers.V[register];
+  };
+
+  ns.operations.writeSoundTimer = (register) => {
+    chip8.timer.sound = chip8.registers.V[register];
+  };
+
+  ns.operations.addToIndex = (register) => {
+    const nextAddress = chip8.registers.I + chip8.registers.V[register];
+    chip8.registers.I = nextAddress;
+    // Set the flag register if I overflows beyond addressable memory
+    if (nextAddress > 0x0FFF) {
+      chip8.registers.V[0xF] = 1;
+    }
+  };
+
+  ns.operations.fontCharacter = (register) => {
+    const hexCode = chip8.registers.V[register] & 0xF;
+    chip8.registers.I = chip8.memoryUtils.fontMap[hexCode];
+  };
+
   ns.fetch = () => {
     // Read 2 bytes, and increment the PC by 2
     fetched[0] = chip8.memory[chip8.cpu.PC];
@@ -354,6 +479,14 @@ chip8.cpu = (() => {
         chip8.debug && console.log('Setting I to', `0x${NNN.toString(16)}`);
         op = () => chip8.cpu.operations.setIndex(NNN);
         break;
+      case 'b':
+        chip8.debug && console.log('Jumping with offset', `0x${NNN.toString(16)}`);
+        op = () => chip8.cpu.operations.jumpWithOffset(NNN, chip8.quirks.jumpWithOffsetAlt ? X : 0);
+        break;
+      case 'c':
+        chip8.debug && console.log('Generating random number, ' `0x${NNN.toString(16)}`);
+        op = () => chip8.cpu.operations.generateRandomNumber(X, NN);
+        break;
       case 'd':
         chip8.debug && console.log('Drawing at', X, ',', Y, '- a sprite of', N, 'rows');
         op = () => chip8.cpu.operations.draw(X, Y, N);
@@ -373,8 +506,7 @@ chip8.cpu = (() => {
 })();
 
 chip8.initCPU = () => {
-  // 1. Set program counter to 0x200
-  // 2. Start the Fetch, decode, execute loop
+  chip8.cpu.PC = Number(0x200);
 }
 
 chip8.romdata = Object.create(null);
@@ -462,8 +594,7 @@ chip8.run = () => {
 
   let fetchedInstruction;
   let decodedOperation;
-
-  chip8.cpu.PC = Number(0x200);
+  chip8.initCPU();
   chip8.display.clear();
   let startTime = NaN;
   const mainLoop = (ts) => {
